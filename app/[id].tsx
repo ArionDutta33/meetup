f50d545d4808486accb7db5d40316691fbdf9132
ArionDutta33 <ariondutta333@outlook.com>
2024-10-31 12:26:00 +0530

    Details page

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,16 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams } from 'expo-router';
 import events from 'assets/events.json';
+import dayjs from 'dayjs';
+
 const EventPage = () => {
   const { id } = useLocalSearchParams();
 
   const event = events.find((event) => event.id === id);
+
+  if (!event) {
+    return <Text>Event not found</Text>;
+  }
   return (
-    <View>
-      <Text>Welcome user id: {id}</Text>
-      <Text>{event.title}</Text>
-    </View>
+    <>
+      <View className="flex-1 gap-3 bg-white p-3">
+        <Stack.Screen
+          options={{ title: event.title, headerBackTitleVisible: false, headerTintColor: 'black' }}
+        />
+        <Image className="aspect-video w-full rounded-xl" source={{ uri: `${event.image}` }} />
+        <Text className="text-3xl font-bold" numberOfLines={2}>
+          {event.title}
+        </Text>
+        <Text className="text-lg font-semibold uppercase text-amber-800">
+          {dayjs(event.datetime).format('ddd, D MMM')}&#183;
+          {dayjs(event.datetime).format('h:mm A')}
+        </Text>
+        <Text className="text-lg " numberOfLines={2}>
+          {event.description}
+        </Text>
+      </View>
+    </>
   );
 };
 
