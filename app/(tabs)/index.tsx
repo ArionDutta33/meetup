f3f8ad370edaa87037d46a46d8aeb71aae924bea
ArionDutta33 <ariondutta333@outlook.com>
2024-10-31 11:31:23 +0530

    Parse and display date

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
 import { Stack } from 'expo-router';
-import { Image, Text, View } from 'react-native';
+import { FlatList, Image, Text, View } from 'react-native';
 import EventListItem from '~/components/EventListItem';
 import events from 'assets/events.json';
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
-      <EventListItem event={events[0]} />
-      <EventListItem event={events[1]} />
-      <EventListItem event={events[2]} />
+      <FlatList
+        className="bg-white"
+        data={events}
+        renderItem={({ item }) => <EventListItem event={item} />}
+      />
     </>
   );
 }
